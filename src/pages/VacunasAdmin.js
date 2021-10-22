787221949bea7bda4f090eb4092a8bbd98ee9584
Bmez1 <daniel.bmez1@gmail.com>
2021-10-22 16:51:54 -0500

    Se modificó el metodo remove de las vacunas

--- src/pages/VacunasAdmin.js
+++ src/pages/VacunasAdmin.js
@@ -3,127 +3,100 @@ import Navbar from "../components/Navbar";
 import APIInvoke from "../utils/APIInvoke";
 import { Link } from 'react-router-dom'
 
-class VacunasAdmin  extends React.Component{
-  
-constructor(props){
-    super(props)
-    this.state={
-        vacunas:[]
-    };
-    this.nmount=false;
-}
+class VacunasAdmin extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            vacunas: []
+        };
 
-async componentDidMount(){    
-    
-    const response=await APIInvoke.invokeGET("/vacunas")
-    this.setState({
-        vacunas: response
-    })  
-    //this.mounted=true;
-
-}
-
-
-async remove(e,vacuna){
-    e.preventDefault();
-    
-    const response=await APIInvoke.invokeDELETE(`/vacunas/${vacuna.id}`) 
-    if(response.id!==0){alert("Registro eliminado")}
-    this.setState({nmount: true});
-  //this.mounted=true;
-} 
-
-async componentDidUpdate(){  
-    
-    if(this.state.nmount===true){
-        const response=await APIInvoke.invokeGET("/vacunas")
-        this.setState({vacunas: response})
-        this.setState({nmount: false});
-       // this.mounted=false
     }
-    
-}
 
-componentWillUnmount(){this.mounted=false}
 
-/*async componentDidUpdate(){
-   
+    async componentDidMount() {
 
-    this.componentDidMount()
- 
-   if(flag ===1){
+        const response = await APIInvoke.invokeGET("/vacunas")
+        this.setState({
+            vacunas: response
+        })
+        //this.mounted=true;
 
-    
-    const response=await APIInvoke.invokeGET("/vacunas")
-    this.setState({
-        vacunas: response
-    })
-    flag=0;
-}
-}*/
+    }
 
-    render(){
 
-        const arregloVacunas= this.state.vacunas
+    async remove(e, vacuna) {
+        e.preventDefault();
 
-        return(
-        <div>
-<Navbar></Navbar>
-<div className=" main container">
-    <div className="row">
-        <div className="col-md-12">
-            <Link to="/vacunas-crear"className="btn btn-light btn-md font-weight-bold ">Agregar Vacuna</Link>
-            <br/><br/>
-            { 
-                arregloVacunas.length ===0 ? <div className="lert alert-warning">No existen registros.</div>:
-                <table className="table table-striped table-hover table-bordered align-middle" display= "flex"  justify-content= "center">
-                <thead>
-                <tr bgcolor="#27BFAE" className="text-white"> 
-                    <th align-middlescope="col">ID</th>
-                    <th scope="col">Vacuna contra</th>
-                    <th scope="col" className="col-md-2">Dosis</th>
-                    <th scope="col">Laboratorio</th>
-                    <th scope="col" className="col-md-2">Nº de Lote</th>
-                    <th scope="col" colSpan="2">Acciones</th>
-                </tr>
-                </thead>
-                <tbody>
-                                {
-                                    arregloVacunas.map(
-                                    vacuna=>
-                <tr key={vacuna.id}>
-                    <td >{vacuna.id}</td>
-                    <td >{vacuna.nombre}</td>
-                    <td>{vacuna.dosis}</td>
-                    
-                    <td>{vacuna.laboratorio}</td>
-                    <td >{vacuna.num_lote}</td>
-                   
-                    <td >
-                    
-                        
-                        <Link to={`/vacunas-editar/${vacuna.id}`} className="btn btn-success btn-sm" title="Editar"><i className="fas fa-sync-alt"></i></Link>
-                       
-                   </td><td>
-                  
-                        <button onClick={ (e)=> this.remove(e, vacuna)} className="btn btn-danger btn-sm" title="Eliminar"><i className="far fa-trash-alt"></i></button>
-                        
-                    </td>
-                </tr>
-                               )
-                                }
-                               
-                            </tbody>
-                            </table>
-                        
-            }
-            
-        </div>
+        const response = await APIInvoke.invokeDELETE(`/vacunas/${vacuna.id}`)
+        this.componentDidMount();
+        if (response.id !== 0) { alert("Registro eliminado"); }
+        //this.mounted=true;
+    }
 
-    </div>
-</div>
-        </div>
+    //componentWillUnmount() { this.mounted = false }
+
+    render() {
+
+        const arregloVacunas = this.state.vacunas
+
+        return (
+            <div>
+                <Navbar></Navbar>
+                <div className=" main container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <Link to="/vacunas-crear" className="btn btn-light btn-md font-weight-bold ">Agregar Vacuna</Link>
+                            <br /><br />
+                            {
+                                arregloVacunas.length === 0 ? <div className="lert alert-warning">No existen registros.</div> :
+                                    <table className="table table-striped table-hover table-bordered align-middle" display="flex" justify-content="center">
+                                        <thead>
+                                            <tr bgcolor="#27BFAE" className="text-white">
+                                                <th align-middlescope="col">ID</th>
+                                                <th scope="col">Vacuna contra</th>
+                                                <th scope="col" className="col-md-2">Dosis</th>
+                                                <th scope="col">Laboratorio</th>
+                                                <th scope="col" className="col-md-2">Nº de Lote</th>
+                                                <th scope="col" colSpan="2">Acciones</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {
+                                                arregloVacunas.map(
+                                                    vacuna =>
+                                                        <tr key={vacuna.id}>
+                                                            <td >{vacuna.id}</td>
+                                                            <td >{vacuna.nombre}</td>
+                                                            <td>{vacuna.dosis}</td>
+
+                                                            <td>{vacuna.laboratorio}</td>
+                                                            <td >{vacuna.num_lote}</td>
+
+                                                            <td >
+
+
+                                                                <Link to={`/vacunas-editar/${vacuna.id}`} className="btn btn-success btn-sm" title="Editar"><i className="fas fa-sync-alt"></i></Link>
+
+                                                            </td><td>
+
+                                                                <button onClick={(e) => this.remove(e, vacuna)} className="btn btn-danger btn-sm" title="Eliminar"><i className="far fa-trash-alt"></i></button>
+
+                                                            </td>
+                                                        </tr>
+                                                )
+                                            }
+
+                                        </tbody>
+                                    </table>
+
+                            }
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         )
     }
 }
--- src/pages/VacunasEditar.js
+++ src/pages/VacunasEditar.js
@@ -10,81 +10,78 @@ class VacunasEditar extends React.Component {
     this.state = {
       id: '',
       nombre: '',
-      dosis:'',
-      viaAplicacion:'',
-      laboratorio:'',
-      num_lote:''
+      dosis: '',
+      viaAplicacion: '',
+      laboratorio: '',
+      num_lote: ''
     };
   }
 
-
-
-  async componentDidMount(){
+  async componentDidMount() {
     document.getElementById("nombre").focus();
-      const vacunaId=this.props.match.params.vacunaId
-      const response=await APIInvoke.invokeGET(`/vacunas/${vacunaId}`)
-      this.setState({
-          id:response.id,
-          nombre:response.nombre,
-          dosis:response.dosis,
-          viaAplicacion:response.viaAplicacion,
-          laboratorio:response.laboratorio,
-          num_lote:response.num_lote
-      })
+    const vacunaId = this.props.match.params.vacunaId
+    const response = await APIInvoke.invokeGET(`/vacunas/${vacunaId}`)
+    this.setState({
+      id: response.id,
+      nombre: response.nombre,
+      dosis: response.dosis,
+      viaAplicacion: response.viaAplicacion,
+      laboratorio: response.laboratorio,
+      num_lote: response.num_lote
+    })
   }
 
- /* handleChange(e){
+  /* handleChange(e){
+     this.setState({
+         nombre: e.target.value
+     })
+ }*/
+
+  handleChange = (e) => {
+    const { name, value } = e.target
+
     this.setState({
-        nombre: e.target.value
-    })
-}*/
+      [name]: value
+    });
 
-handleChange=(e) =>{
-  const {name, value}=e.target
-  
-  this.setState({
-    [name]:value
-  });
+  }
 
-}
-
-handleSubmit =(ev)=>{
-  ev.preventDefault();
- 
- 
-  this.edit()
-}
+  handleSubmit = (ev) => {
+    ev.preventDefault();
+    this.edit()
+  }
 
   async edit() {
-      const data2={
-          id:this.state.id,
-          nombre: this.state.nombre.trim(),
-          dosis: this.state.dosis.trim(),
-          viaAplicacion: this.state.viaAplicacion.trim(),
-          laboratorio: this.state.laboratorio.trim(),
-          num_lote: this.state.num_lote.trim()
-      }
-      if (data2.nombre===""||data2.dosis===""||data2.viaAplicacion===""||data2.laboratorio===""||data2.num_lote===""){
-        alert("Debe diligenciar todos los espacios del formulario");
-    }else{
-      const response=await APIInvoke.invokePUT(`/vacunas`, data2)
+    const data2 = {
+      id: this.state.id,
+      nombre: this.state.nombre.trim(),
+      dosis: this.state.dosis.trim(),
+      viaAplicacion: this.state.viaAplicacion.trim(),
+      laboratorio: this.state.laboratorio.trim(),
+      num_lote: this.state.num_lote.trim()
+    }
+    if (data2.nombre === "" || data2.dosis === "" || data2.viaAplicacion === "" || data2.laboratorio === "" || data2.num_lote === "") {
+      alert("Debe diligenciar todos los espacios del formulario");
+    } else {
+      const response = await APIInvoke.invokePUT(`/vacunas`, data2)
 
-      document.getElementById("nombre").value="";
-      document.getElementById("dosis").value="";
-      document.getElementById("viaAplicacion").value="";
-      document.getElementById("laboratorio").value="";
-      document.getElementById("num_lote").value="";
+      document.getElementById("nombre").value = "";
+      document.getElementById("dosis").value = "";
+      document.getElementById("viaAplicacion").value = "";
+      document.getElementById("laboratorio").value = "";
+      document.getElementById("num_lote").value = "";
 
       if (response.id !== 0) {
         this.props.history.push(`/vacunas`)
         alert("Cambio exitoso");
-      }else{
+      } else {
         alert("Cambio fallo");
       }
-        //window.location='/vacunas';
-   } }
+      //window.location='/vacunas';
+    }
+  }
 
- 
+
 
   render() {
     return (
@@ -92,94 +89,93 @@ handleSubmit =(ev)=>{
         <Navbar></Navbar>
 
         <div id="div2">
-        <section id="superior">
+          <section id="superior">
             <h1 id="div3" >EDITAR VACUNA</h1>
-        </section>
-    </div>
-  
-    <form onSubmit={this.handleSubmit}>
-    <section id="contenido">
-        <div className="container border" id="div4">
-            <div className="row">
-              <div className="col">
+          </section>
+        </div>
+
+        <form onSubmit={this.handleSubmit}>
+          <section id="contenido">
+            <div className="container border" id="div4">
+              <div className="row">
                 <div className="col">
+                  <div className="col">
                     <label htmlFor="nombre" className="form-label">Nombre Vacuna</label>
-                    <input type="nombre" name="nombre" className="form-control" id="nombre" aria-describedby="nombreDesc" 
-                     value={this.state.nombre}   onChange={this.handleChange} />
+                    <input type="nombre" name="nombre" className="form-control" id="nombre" aria-describedby="nombreDesc"
+                      value={this.state.nombre} onChange={this.handleChange} />
                     <div id="nombreDesc" className="form-text">Máximo 100 caracteres permitidos.</div>
                   </div>
-              </div>
-              <div className="col">
-                <div className="col">
-                    <label  htmlFor="nombre" className="form-label">Laboratorio</label>
-                    <input  type="laboratorio" name="laboratorio" className="form-control" id="laboratorio" aria-describedby="nombreDesc"
-                    value={this.state.laboratorio}   onChange={this.handleChange} />
-                    <div id="nombreDesc" className="form-text">Máximo 100 caracteres permitidos.</div>
                 </div>
-              </div>   
+                <div className="col">
+                  <div className="col">
+                    <label htmlFor="nombre" className="form-label">Laboratorio</label>
+                    <input type="laboratorio" name="laboratorio" className="form-control" id="laboratorio" aria-describedby="nombreDesc"
+                      value={this.state.laboratorio} onChange={this.handleChange} />
+                    <div id="nombreDesc" className="form-text">Máximo 100 caracteres permitidos.</div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="row" id="div1" >
+                <div className="col">
+                  <div className="col">
+                    <label htmlFor="nombre" className="form-label">Dosis</label>
+                    <br />
+                    <select defaultValue="" type="dosis" name="dosis" id="dosis" className="form-select" aria-label="Default select example"
+                      checked={this.state.dosis} onChange={this.handleChange}>
+                      <option value="" >---- Seleccione ----</option>
+                      <option value="Recien Nacido">Recien Nacido</option>
+                      <option value="Única">Única</option>
+                      <option value="Primera">Primera</option>
+                      <option value="Segunda">Segunda</option>
+                      <option value="Tercera">Tercera</option>
+                      <option value="Primer Refuerzo">Primer Refuerzo</option>
+                      <option value="Segundo Refuerzo">Segundo Refuerzo</option>
+                      <option value="Anual">Anual</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="col">
+                  <div className="col">
+                    <label htmlFor="nombre" className="form-label">Nº de Lote</label>
+                    <input type="num_lote" id="num_lote" name="num_lote" className="form-control" aria-describedby="nombreDesc"
+                      value={this.state.num_lote} onChange={this.handleChange} />
+                    <div id="nombreDesc" className="form-text">Máximo 20 caracteres permitidos.</div>
+                  </div>
+                </div>
+                <div className="col">
+                  <div className="col">
+                    <label htmlFor="nombre" className="form-label">Via de Aplicación</label>
+                    <br />
+                    <select defaultValue="" type="viaAplicacion" name="viaAplicacion" id="viaAplicacion" className="form-select" aria-label="Default select example"
+                      checked={this.state.viaAplicacion} onChange={this.handleChange} >
+                      <option value="" >---- Seleccione ----</option>
+                      <option value="Oral">Oral</option>
+                      <option value="Intramuscular">Intramuscular</option>
+                      <option value="Intravenosa">Intravenosa</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
             </div>
 
-            <div className="row" id="div1" >
-                <div className="col">
-                    <div className="col">
-                        <label  htmlFor="nombre" className="form-label">Dosis</label>
-                        <br/>
-                        <select defaultValue="" type="dosis" name="dosis"  id="dosis" className="form-select" aria-label="Default select example"
-                        checked={this.state.dosis}   onChange={this.handleChange}>
-                            <option value="" >---- Seleccione ----</option>
-                            <option value="Recien Nacido">Recien Nacido</option>
-                            <option value="Única">Única</option>
-                            <option value="Primera">Primera</option>
-                            <option value="Segunda">Segunda</option>
-                            <option value="Tercera">Tercera</option>
-                            <option value="Primer Refuerzo">Primer Refuerzo</option>
-                            <option value="Segundo Refuerzo">Segundo Refuerzo</option>
-                            <option value="Anual">Anual</option>
-                          </select>
-                        
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="col">
-                        <label htmlFor="nombre" className="form-label">Nº de Lote</label>
-                        <input type="num_lote" id="num_lote"  name="num_lote"  className="form-control"  aria-describedby="nombreDesc"
-                         value={this.state.num_lote}   onChange={this.handleChange} />
-                        <div id="nombreDesc" className="form-text">Máximo 20 caracteres permitidos.</div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="col">
-                        <label htmlFor="nombre" className="form-label">Via de Aplicación</label>
-                        <br/>
-                        <select  defaultValue="" type="viaAplicacion" name="viaAplicacion" id="viaAplicacion" className="form-select" aria-label="Default select example"
-                        checked={this.state.viaAplicacion}   onChange={this.handleChange} >
-                            <option value="" >---- Seleccione ----</option>
-                            <option value="Oral">Oral</option>
-                            <option value="Intramuscular">Intramuscular</option>
-                            <option value="Intravenosa">Intravenosa</option>
-                          </select>
-                    </div>
-                </div>
-              </div>
-          </div>
-         
-        <div className="container" id="div5" >
-            <div id="div6">
+            <div className="container" id="div5" >
+              <div id="div6">
                 <Link to="/vacunas" type="submit" className="btn btn-danger">Cancelar</Link>
                 &nbsp;&nbsp;
-                
-                <button href="/vacunas" type="submit" className="btn btn-primary" >Actualizar</button>  
-            </div>   
-        </div>
-        </section>
-    </form>
+
+                <button href="/vacunas" type="submit" className="btn btn-primary" >Actualizar</button>
+              </div>
+            </div>
+          </section>
+        </form>
 
 
 
 
 
 
-        
+
       </div>
     );
   }
